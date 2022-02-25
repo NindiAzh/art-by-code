dd60aa135d67526aefae0d17f7eb5baad0ee3737
NindiAzh <nindyk4@gmail.com>
2022-02-25 22:20:09 +0700

    jum25feb22

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,10 @@ const Hero = () => {
             <p className="inline-block py-4 px-5 text-gray-500 mb-3 text-2xl">
                 Kami adalah <span className='text-puerto-rico-500 font-bold font-mons'>Art By Code <span>&#10072;</span> ABC</span></p>
             <h1 className="font-black text-5xl mb-4 px-4 w-4/5 leading-snug font-nuni">Ayo ciptakan sesuatu yang indah dari sebuah kode</h1>
-            <Button2>mau tau lebih banyak</Button2>
+            <button className="bg-puerto-rico-500 text-white-50 text-sm mx-4 py-3 px-6 inline-flex items-center rounded-md mt-6">
+              mau tau lebih banyak
+          </button>
+
             </div>
             <div className='py-20 pr-10 w-2/3'>
                 <Image className='' src={imgHome} alt="imghome" />
